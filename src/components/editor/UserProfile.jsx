cc288cd5d52eb13452df1c40d6d4e977131db99c
Tom Main <tommain1498@gmail.com>
2024-09-09 20:05:18 +0100

    fixed user details not showing on no exist

--- src/components/editor/UserProfile.jsx
+++ src/components/editor/UserProfile.jsx
@@ -1,10 +1,10 @@
 export default function UserProfile(props){
     return(
         <div className="userProfile">
-            {props.userDetails.avatar ? <div className="userAvatar"></div> :  <div className="userAvatar"></div>}
+            {props.userDetails?.avatar ? <div className="userAvatar"></div> :  <div className="userAvatar"></div>}
             <div className="details">
-                {props.userDetails.name ? <h2>{props.userDetails.name}</h2> : <div className="empty emptyDetails emptyName"></div>}
-                {props.userDetails.email ? <p className="bodySmall">{props.userDetails.email}</p> :<div className=" empty emptyDetails emptyEmail"></div>}
+                {props.userDetails?.name ? <h2>{props.userDetails.name}</h2> : <div className="empty emptyDetails emptyName"></div>}
+                {props.userDetails?.email ? <p className="bodySmall">{props.userDetails.email}</p> :<div className=" empty emptyDetails emptyEmail"></div>}
             </div>
         </div>
     )
