8c8a12b6227d0072339db5990d360e8169cdd7a0
Tom Main <tommain1498@gmail.com>
2024-09-22 14:08:56 +0100

    installed toastify

--- src/pages/TestProfilePage.jsx
+++ src/pages/TestProfilePage.jsx
@@ -1,5 +1,10 @@
 //test page which displays the users username based on the url
 
+import React from 'react';
+import SaveEditBar from '../components/editor/SaveEditBar';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 export default function TestProfilePage(){
 
     //get username from url
@@ -7,9 +12,21 @@ export default function TestProfilePage(){
     const username = url.split("/").pop();
     console.log(username);
 
+    function fireToast(){
+      
+        toast.loading("Saved Successfully", {
+            autoClose: 2000,
+            position: "top-center",
+        })
+    }
+
     return(
         <div>
+            <ToastContainer />
             <h1>{username}</h1>
+            <SaveEditBar 
+                save={fireToast}
+            />
         </div>
     )
 }
