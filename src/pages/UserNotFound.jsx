fc5e5061dd8c95367627e9f8a9a71d01746d9674
Tom Main <tommain1498@gmail.com>
2024-10-26 20:40:45 +0100

    updated share nav linking

--- src/pages/UserNotFound.jsx
+++ src/pages/UserNotFound.jsx
@@ -1,6 +1,6 @@
 import {React, useEffect, useState} from 'react';
 import axios from 'axios';
-import ShareNav from '../components/sharePage/shareNav';
+import ShareNav from '../components/sharePage/ShareNav.jsx';
 import NavCard from '../components/preview/NavCard';
 
 export default function SharePage(){
